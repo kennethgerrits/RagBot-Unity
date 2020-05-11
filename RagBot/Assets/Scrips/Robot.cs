91ec7feb98f579c21ba69e8357b89301b651a85c
Kenneth Gerrits <kennethgerrits@gmail.com>
2020-05-11 16:11:47 +0200

    linerenderer

--- RagBot/Assets/Scrips/Robot.cs
+++ RagBot/Assets/Scrips/Robot.cs
@@ -8,6 +8,7 @@ public class Robot : MonoBehaviour
     private Vector3 _initialPosition;
     private Rigidbody2D _rigidBody2D;
     private SpriteRenderer _spriteRenderer;
+    private LineRenderer _lineRenderer;
     private bool _robotWasLaunched;
 
     [SerializeField] private float _launchPower = 250;
@@ -17,6 +18,7 @@ public class Robot : MonoBehaviour
     {
         _rigidBody2D = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _lineRenderer = GetComponent<LineRenderer>();
         _initialPosition = transform.position;
     }
 
@@ -28,6 +30,10 @@ public class Robot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        _lineRenderer.SetPosition(0, transform.position);
+        _lineRenderer.SetPosition(1, _initialPosition);
+
         if (transform.position.y > 10 ||
             transform.position.y < -10 ||
             transform.position.x > 30 ||
@@ -42,6 +48,7 @@ public class Robot : MonoBehaviour
     private void OnMouseDown()
     {
         _spriteRenderer.color = Color.red;
+        _lineRenderer.enabled = true;
     }
 
     private void OnMouseUp()
@@ -52,6 +59,8 @@ public class Robot : MonoBehaviour
         _rigidBody2D.AddForce(directionToInitialPosition * _launchPower);
         _rigidBody2D.gravityScale = 1;
         _robotWasLaunched = true;
+
+        _lineRenderer.enabled = false;
     }
 
     private void OnMouseDrag()
