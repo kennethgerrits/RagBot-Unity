249ab687bc6883498df67cda0ce5aa73d93cecb7
Kenneth Gerrits <kennethgerrits@gmail.com>
2020-05-11 14:42:29 +0200

    Bird script start

---
+++ RagBot/Assets/Scrips/Robot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour
+{
+
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;   
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
