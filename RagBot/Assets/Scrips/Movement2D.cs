ef466ed146ddd5220cc2acd435aa1a35852a74ff
Kenneth Gerrits <kennethgerrits@gmail.com>
2020-05-12 19:47:25 +0200

    added restriction where ragbot isn't allowed to jump in mid-air

--- RagBot/Assets/Scrips/Movement2D.cs
+++ RagBot/Assets/Scrips/Movement2D.cs
@@ -6,6 +6,7 @@ public class Movement2D : MonoBehaviour
 {
     private Robot _robot;
     private Rigidbody2D _rigidbody2D;
+    private bool _isGrounded;
 
     [SerializeField] private float _jumpPower = 6f;
     [SerializeField] private float _moveSpeed = 5f;
@@ -32,10 +33,18 @@ public class Movement2D : MonoBehaviour
         Vector3 movementH = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movementH * Time.deltaTime * _moveSpeed;
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && _isGrounded)
         {
             _rigidbody2D.velocity = Vector2.up * _jumpPower;
+            _isGrounded = false;
+        }
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            _isGrounded = true;
         }
     }
 }
